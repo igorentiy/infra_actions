18d05f3ae68aa4e61f5e0b63cf84782a4703eb41
Игорь Литвиненко <marcato1407@yandex.ru>
2022-10-27 10:11:16 +0300

    test with flake8

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
+    """First page."""
     return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
+    """Second page."""
     return HttpResponse('А это вторая страница')
--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,4 +1,4 @@
-"""infra_project URL Configuration
+"""infra_project URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
